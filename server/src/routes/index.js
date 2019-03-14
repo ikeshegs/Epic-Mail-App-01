41b26cb45b9024c0aa784ab1f5e2b2f981be4430
ikeshegs <ikeshegs@gmail.com>
2019-03-14 15:08:19 +0100

    bug(modify auth links): Add middlewares to my signup and login links - created a folder called Middlewares - created a file called validator - created an object, signup and login validator methods
    [#164627862]

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import userController from '../controllers/userController';
 import messageController from '../controllers/messageController';
+import validator from '../middlewares/validator';
 
 // set router
 const router = express.Router();
 
 // User router
-router.post('/api/v1/createuser', userController.createUser);
-router.get('/api/v1/allusers', userController.getUsers);
-router.post('/api/v1/signinuser', userController.signinUser);
+router.post('/api/v1/auth/signup', validator.signupValidator, userController.createUser);
+router.post('/api/v1/auth/login', validator.loginValidator, userController.signinUser);
 
 // Messages
-router.post('/api/v1/createmsg', messageController.createMsg);
-router.get('/api/v1/receivedmsg', messageController.receiveMsg);
-router.get('/api/v1/sentmsg', messageController.sentMsg);
-router.get('/api/v1/unreadmsg', messageController.unreadMsg);
-router.get('/api/v1/specificemail', messageController.specificEmail);
-router.delete('/api/v1/deleteemail', messageController.deleteFromInbox);
+router.post('/api/v1/messages', messageController.createMsg);
+router.get('/api/v1/messages', messageController.receiveMsg);
+router.get('/api/v1/messages/sent', messageController.sentMsg);
+router.get('/api/v1/messages/unread', messageController.unreadMsg);
+router.get('/api/v1/messages/:id', messageController.specificEmail);
+router.delete('/api/v1/messages/:id', messageController.deleteFromInbox);
 
 export default router;
