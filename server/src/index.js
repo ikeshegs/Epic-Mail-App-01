22a54792b3a48a4ba21e48876fa308b7b7fe4827
ikeshegs <ikeshegs@gmail.com>
2019-03-14 10:00:19 +0100

    bg: Modified injex.js and add procfile

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './src/routes/index';
+import router from 'routes/index';
 import cors from 'cors';
 
 // initialize express
