49252ea5c37eaf668af64d7526595af28cfd9513
ikeshegs <ikeshegs@gmail.com>
2019-03-15 04:22:30 +0100

    chore(documentation): Modify READme doc and API documentation - Added more information to readme file - created API documentation using postman - Added swagger.yaml file - create route for API url [#164650631]

--- build/index.js
+++ build/index.js
@@ -4,14 +4,21 @@ var _express = _interopRequireDefault(require("express"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 
-var _index = _interopRequireDefault(require("./routes/index"));
-
 var _cors = _interopRequireDefault(require("cors"));
+
+var _yamljs = _interopRequireDefault(require("yamljs"));
+
+var _swaggerUiExpress = _interopRequireDefault(require("swagger-ui-express"));
+
+var _index = _interopRequireDefault(require("./routes/index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// initialize express
-var app = (0, _express.default)(); // configure bodyParser for incoming requests
+var swaggerDoc = _yamljs.default.load("".concat(__dirname, "/../../swagger.yaml")); // initialize express
+
+
+var app = (0, _express.default)();
+app.use('/api-docs', _swaggerUiExpress.default.serve, _swaggerUiExpress.default.setup(swaggerDoc)); // configure bodyParser for incoming requests
 
 app.use(_bodyParser.default.json());
 app.use(_bodyParser.default.urlencoded({
