80f8745a550f443f7cba4b0b28b214a703b951c2
ikeshegs <ikeshegs@gmail.com>
2019-03-15 02:19:47 +0100

    bug(middleware): build files with babel
    [#164637623]

--- build/middlewares/validator.js
+++ build/middlewares/validator.js
@@ -8,36 +8,31 @@ var validator = {
   signupValidator: function signupValidator(req, res, next) {
     if (!req.body.firstname || req.body.firstname.trim().length < 1) {
       return res.status(400).send({
-        success: false,
-        message: 'Firstname is required'
+        error: 'Firstname is required'
       });
     }
 
     if (!req.body.lastname || req.body.lastname.trim().length < 1) {
       return res.status(400).send({
-        success: false,
-        message: 'Lastname is required'
+        error: 'Lastname is required'
       });
     }
 
     if (!req.body.email || req.body.email.trim().length < 1) {
       return res.status(400).send({
-        success: false,
-        message: 'Email address is required'
+        error: 'Email address is required'
       });
     }
 
     if (!req.body.password || req.body.password.trim().length < 1) {
       return res.status(400).send({
-        success: false,
-        message: 'Password is required'
+        error: 'Password is required'
       });
     }
 
     if (!req.body.phone || req.body.phone.trim().length < 1) {
       return res.status(400).send({
-        success: false,
-        message: 'Phone Number is required'
+        error: 'Phone Number is required'
       });
     }
 
